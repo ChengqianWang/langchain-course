071714f45c9e5c9231c11774a6c624e7524e4794
Chengqian_Wang <chengqiandaily@outlook.com>
2026-04-17 22:55:24 +0100

    built with TavilySearch

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_qwq import ChatQwen
+from langchain_tavily import TavilySearch
+
+
+
+llm = ChatQwen(model="qwen3-max")
+tools = [TavilySearch()]
+agent = create_agent(llm, tools = tools)
+
+
+
 def main():
     print("Hello from langchain-course!")
-
+    result = agent.invoke({"messages": HumanMessage(content="search for three job positions for an ai engineer using langchain in linkedin in dublin and list their details")})
+    print(result)
 
 if __name__ == "__main__":
     main()
