0045c9c250ae8b489c193d42b87c29a8bc02e44b
Chengqian_Wang <chengqiandaily@outlook.com>
2026-04-18 15:43:47 +0100

    back up the code

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
@@ -8,18 +11,33 @@ from langchain_core.messages import HumanMessage
 from langchain_qwq import ChatQwen
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
 
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema ofr agent response with answer and resources"""
+
+    answer:str = Field(description="the agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
 llm = ChatQwen(model="qwen3-max")
 tools = [TavilySearch()]
-agent = create_agent(llm, tools = tools)
-
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content="search for three job positions for an ai engineer using langchain in linkedin in dublin and list their details")})
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="search for three job positions for an ai engineer using langchain in linkedin in dublin and list their details"
+            )
+        }
+    )
     print(result)
+
 
 if __name__ == "__main__":
     main()
